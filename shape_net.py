dda182f2766f5a07e7c373c02ff08af4959fc1a0
Alf-13 <alfreisinger@gmail.com>
2025-04-13 12:48:34 -0500

    Added comments and tweaks to shape_net

--- shape_net.py
+++ shape_net.py
@@ -13,8 +13,8 @@ layers = 5 #number of hidder layers
 hidden_size = 22 #size of the first hidden layer
 taper = 1 #coefficient that changes the size of subsequent hidden layers
 epochs = 1 #number of times the program goes through all iterations
-iterations = 4000
-alpha = 0.00005 #learning rate
+iterations = 500
+alpha = 0.00003 #learning rate
 
 #initailize arrays
 error_pool = np.empty((epochs))
@@ -44,38 +44,38 @@ for j in range(epochs):
     for y in range(iterations):
         error = 0
         for inc in range(1000):
-            rand_in = [random.randrange(1,101),random.randrange(1,101)]
+            rand_in = [random.randrange(1,101),random.randrange(1,101)] #randomly generate base and height
             base = max(rand_in)
             height = min(rand_in)
-            shape_select = random.randint(0,2)
+            shape_select = random.randint(0,2) #randomly select shape type (rectangle, triangle, circle)
 
-            if shape_select == 0:
+            if shape_select == 0: #assign area based on shape type with a small random error
                 A = base*height*(1+(2*random.random()-1)/100)
             elif shape_select == 1:
                 A = 0.5*base*height*(1+(2*random.random()-1)/100)
             else:
-                A = np.pi*0.25*base*height*(1+(2*random.random()-1)/100)
+                A = np.pi*base*height*(1+(2*random.random()-1)/100)
             layer_in[0,0],layer_in[0,1],layer_in[0,2] = base,height,A
-            layer_in /= 100
+            layer_in /= 100 #normalize input
 
-            for z in range(1,(layers+1)):
+            for z in range(1,(layers+1)): #forward propagation
                 if z == 1:
                     layer_1 = activation(np.dot(layer_in,weight_in_1))
                 else:
                     alayer = f'layer_{z}'
                     xlayer = f'layer_{z-1}'
                     xweight = f'weight_{z-1}_{z}'
-                    locals()[alayer] = activation(np.dot(locals()[xlayer],locals()[xweight]))
+                    locals()[alayer] = activation(np.dot(locals()[xlayer],locals()[xweight])) #activation function
 
             xlayer = f'layer_{layers}'
             xweight = f'weight_{layers}_out'
-            layer_out = 1/(1+np.exp(-1*np.dot(locals()[xlayer],locals()[xweight])))
+            layer_out = 1/(1+np.exp(-1*np.dot(locals()[xlayer],locals()[xweight]))) #output layer activation function
             actual = np.zeros((1,3))
             actual[0,shape_select] = 1
-            error += np.sum((layer_out-actual)**2)
+            error += np.sum((layer_out-actual)**2) #calculate error
             delta_out = layer_out-actual
 
-            for k in reversed(range(1,(layers+1))):
+            for k in reversed(range(1,(layers+1))): #backpropagation
                 if k  ==  layers:
                     xdelta = f'delta_{k}'
                     xweight = f'weight_{k}_out'
@@ -88,7 +88,7 @@ for j in range(epochs):
                     xlayer = f'layer_{k}'
                     locals()[xdelta] = np.dot(locals()[ydelta],locals()[xweight].T)*act_dev(locals()[xlayer])
 
-            for t in range(layers+1):
+            for t in range(layers+1): #update weights
                 if t  ==  0:
                     xweight = f'weight_in_1'
                     xlayer = f'layer_in'
@@ -110,37 +110,37 @@ for j in range(epochs):
 n_test = 1000
 correct = 0
 
-for it in range(n_test):
+for it in range(n_test): #test the network
     rand_in = [random.randrange(1,101),random.randrange(1,101)]
     base = max(rand_in)
     height = min(rand_in)
-    shape_select = random.randint(0,2)
+    shape_select = random.randint(0,2) #select shape
 
-    if shape_select == 0:
+    if shape_select == 0: #assign area
         A = base*height#*(1+(2*random.random()-1)/100)
     elif shape_select == 1:
         A = 0.5*base*height#*(1+(2*random.random()-1)/100)
     else:
-        A = np.pi*0.25*base*height#*(1+(2*random.random()-1)/100)
-    layer_in[0,0],layer_in[0,1],layer_in[0,2] = base,height,A
-    layer_in /= 100
+        A = np.pi*base*height#*(1+(2*random.random()-1)/100)
+    layer_in[0,0],layer_in[0,1],layer_in[0,2] = base,height,A #input values
+    layer_in /= 100 #normalize
 
-    for z in range(1,(layers+1)):
+    for z in range(1,(layers+1)): #forward propagation
         if z == 1:
             layer_1 = activation(np.dot(layer_in,weight_in_1))
         else:
             alayer = f'layer_{z}'
             xlayer = f'layer_{z-1}'
             xweight = f'weight_{z-1}_{z}'
-            locals()[alayer] = activation(np.dot(locals()[xlayer],locals()[xweight]))
+            locals()[alayer] = activation(np.dot(locals()[xlayer],locals()[xweight])) #activation function
 
     xlayer = f'layer_{layers}'
     xweight = f'weight_{layers}_out'
-    layer_out = 1/(1+np.exp(-1*np.dot(locals()[xlayer],locals()[xweight])))
+    layer_out = 1/(1+np.exp(-1*np.dot(locals()[xlayer],locals()[xweight]))) #output layer activation function
     actual = np.zeros((1,3))
-    actual[0,shape_select] = 1
+    actual[0,shape_select] = 1 #output
 
-    if np.argmax(layer_out) == np.argmax(actual):
+    if np.argmax(layer_out) == np.argmax(actual): #check if output is correct
         correct += 1
 
 print(f'Correct classifications: {round(correct*100/n_test,2)}%')
